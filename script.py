ed2821920fea56b0404dca0d7849d2efd2d5177d
Daniel Marchena Parreira <danielmapar@gmail.com>
2021-12-27 05:47:10 -0500

    Initial commit

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ devices = api.get_device_details()
 
 def change_light(device):
     device.set_color([11, 102, 35])
-    device.set_brightness(100)
+    device.set_brightness(10)
     #device.set_color_temperature(10)
 
 for device in devices:
