1181580cba3250cf9d7efc967b2b9cc936d45a8c
Daniel Marchena Parreira <danielmapar@gmail.com>
2021-12-27 06:21:38 -0500

    Initial commit

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import sengled
 from dotenv import load_dotenv
 from threading import Thread
+import random
+import time
 
 load_dotenv()
 
@@ -8,13 +10,27 @@ api = sengled.api_from_env()
 
 devices = api.get_device_details()
 
-def change_light(device):
-    device.set_color([11, 102, 35])
-    device.set_brightness(10)
+
+
+def change_light(device, color):
+    device.set_color(color)
+    device.set_brightness(random.randrange(1, 100))
     #device.set_color_temperature(10)
 
-for device in devices:
-    print(device)
-    Thread(target=change_light, args=(device, )).start()
+while (True):
+    time.sleep(10)
+    red = random.randrange(1, 255)
+    green = random.randrange(1, 255)
+    blue = random.randrange(1, 255)
+    color = [red, green, blue]
+    threads = []
+    for device in devices:
+        threads.append(Thread(target=change_light, args=(device, color, )))
+    
+    for thread in threads:
+        thread.start()
+    
+    for thread in threads:
+        thread.join()
     
 
